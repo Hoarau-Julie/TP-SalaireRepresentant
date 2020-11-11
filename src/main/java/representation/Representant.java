fd2526fbdb89e73b5b5868ae6bb81133af5850a5
Hoarau Julie <Hoarau Julie@192.168.1.20>
2020-11-11 13:58:07 +0100

    Final TP

--- src/main/java/representation/Representant.java
+++ src/main/java/representation/Representant.java
@@ -1,5 +1,7 @@
 package representation;
 
+import java.util.Optional;
+
 public class Representant {
 
 	private final int numero;
@@ -7,57 +9,88 @@ public class Representant {
 	private final String prenom;
 	private String adresse;
 	private float salaireFixe;
+        private ZoneGeographique secteur;
+        private float[] CAMensuel = new float[12];
+        
 
-	public Representant(int numero, String nom, String prenom, ZoneGeographique secteur) {
+	public Representant(int numero, String nom, String prenom, ZoneGeographique secteur) 
+        {
 		this.numero = numero;
 		this.nom = nom;
 		this.prenom = prenom;
+                this.secteur=secteur;
 	}
 
-	public int getNumero() {
+	public int getNumero() 
+        {
 		return numero;
 	}
 
-	public String getNom() {
+	public String getNom() 
+        {
 		return nom;
 	}
 
-	public String getPrenom() {
+	public String getPrenom() 
+        {
 		return prenom;
 	}
 
-	public String getAdresse() {
-		return adresse;
+        public Optional<String> getAdresse() 
+        {
+		return Optional.ofNullable(adresse);
 	}
-
-	public void setAdresse(String adresse) {
+        
+        
+	public void setAdresse(String adresse) 
+        {
 		this.adresse = adresse;
 	}
 
-	public float getSalaireFixe() {
+	public float getSalaireFixe() 
+        {
 		return salaireFixe;
 	}
 
-	public void setSalaireFixe(float salaireFixe) {
+	public void setSalaireFixe(float salaireFixe) 
+        {
 		this.salaireFixe = salaireFixe;
 	}
 
-	public ZoneGeographique getSecteur() {
-		// TODO: Implémenter cette méthode
-		throw new UnsupportedOperationException("Pas encore implémenté");
+	public ZoneGeographique getSecteur() 
+        {
+		return this.secteur;
 	}
 
-	public void setSecteur(ZoneGeographique secteur) {
-		// TODO: Implémenter cette méthode
-		throw new UnsupportedOperationException("Pas encore implémenté");
+	public void setSecteur(ZoneGeographique secteur) 
+        {
+		this.secteur = secteur;
 	}
+        public float[] getCAMensuel()
+        {
+            return this.CAMensuel;
+        }
 
+        public void setCAMensuel(float[] CAmensuel)
+        {
+            if (CAmensuel.length<=12)
+            {
+                this.CAMensuel=CAmensuel;
+            }
+            else
+            {
+                throw new IllegalArgumentException("Le CA ne peut contenir que 12 valeurs.");
+            }
+        }
+                
 	/**
 	 * Enregistre le CA de ce représentant pour un mois donné. 
 	 * @param mois le numéro du mois (de 0 à 11)
 	 * @param montant le CA réalisé pour ce mois (positif ou nul)
 	 **/
-	public void enregistrerCA(int mois, float montant) {
+        
+        public void registerCA(int mois, float montant) 
+        {
 		// vérifier les paramètres
 		if (mois < 0 || mois > 11) {
 			throw new IllegalArgumentException("Le mois doit être compris entre 0 et 11");
@@ -65,8 +98,7 @@ public class Representant {
 		if (montant < 0) {
 			throw new IllegalArgumentException("Le montant doit être positif ou null");
 		}
-		// TODO: Implémenter cette méthode
-		throw new UnsupportedOperationException("Pas encore implémenté");
+		this.CAMensuel[mois]=montant;
 	}
 
 	/**
@@ -76,8 +108,13 @@ public class Representant {
 	 * @return le salaire pour ce mois, tenant compte du salaire fixe, de l'indemnité repas, et du pourcentage sur CA
 	 */
 	public float salaireMensuel(int mois, float pourcentage) {
-		// TODO: Implémenter cette méthode
-		throw new UnsupportedOperationException("Pas encore implémenté");
+		if (mois < 0 || mois > 11) {
+			throw new IllegalArgumentException("Le mois doit être compris entre 0 et 11");
+		}
+		if (pourcentage < 0) {
+			throw new IllegalArgumentException("Le montant doit être positif ou null");
+		}
+		return this.salaireFixe + this.secteur.getIndemniteRepas() + this.CAMensuel[mois]*pourcentage;
 	}
 
 	@Override
