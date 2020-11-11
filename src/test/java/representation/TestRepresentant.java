d6c710b8be7cc71439d34b87ece22f05a3b9e366
Hoarau Julie <Hoarau Julie@192.168.1.20>
2020-11-11 13:58:57 +0100

    Final TP (tests)

--- src/test/java/representation/TestRepresentant.java
+++ src/test/java/representation/TestRepresentant.java
@@ -1,6 +1,7 @@
 package representation;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,7 +15,8 @@ public class TestRepresentant {
 	private ZoneGeographique occitanie;
 	
 	@BeforeEach
-	public void setUp() {
+	public void setUp() 
+        {
 		// Initialiser les objets utilisés dans les tests
 		occitanie = new ZoneGeographique(1, "Occitanie");
 		occitanie.setIndemniteRepas(INDEMNITE_OCCITANIE);
@@ -24,11 +26,12 @@ public class TestRepresentant {
 	}
 	
 	@Test
-	public void testSalaireMensuel() {
+	public void testSalaireMensuel() 
+        {
 		float CA = 50000f;
 		float POURCENTAGE= 0.1f; // 10% de pourcentage sur CA
 		// On enregistre un CA pour le mois 0 (janvier)
-		r.enregistrerCA(0, CA);
+		r.registerCA(0, CA);
 		
 		// On calcule son salaire pour le mois 0 avec 10% de part sur CA
 		float salaire = r.salaireMensuel(0, POURCENTAGE);
@@ -43,12 +46,37 @@ public class TestRepresentant {
 			// Marge d'erreur tolérée
 			0.001,
 			// Message si erreur
-			"Le salaire mensuel est incorrect"
-		); 
+			"Le salaire mensuel est incorrect"); 
 	}
-
-	@Test
-	public void testCAParDefaut() {
+        
+        @Test
+        public void testSalaireMois()
+        {
+		try 
+                {
+			// On enregistre 12ème mois
+			// On s'attend à recevoir une exception
+			r.salaireMensuel(12, FIXE_BASTIDE);
+			// Si on arrive ici, c'est une erreur, le test doit échouer
+                        // On force l'échec du test
+			fail("Le mois 12 doit lever une exception"); 			
+                    } 
+                catch (IllegalArgumentException e) 
+                {
+			// Si on arrive ici cela correspond au comportement attendu : c'est normal.
+		}
+        }
+        
+        //Test adresse vide
+        @Test
+        public void testAdresseNulle(){
+           assertTrue(r.getAdresse().isEmpty());
+        }
+        
+        
+        @Test
+	public void testCAParDefaut() 
+        {
 		float POURCENTAGE= 0.1f; // 10% de pourcentage sur CA
 		
 		// On n'enregistre aucun CA
@@ -64,24 +92,101 @@ public class TestRepresentant {
 			FIXE_BASTIDE + INDEMNITE_OCCITANIE, 
 			salaire, 
 			0.001,
-			"Le CA n'est pas correctement initialisé"
-		);
+			"Le CA n'est pas correctement initialisé");
 	}
 
+        @Test
+	public void testPourcentageNegatifImpossible() 
+        {
+		
+		try {
+                        float POURCENTAGE= -0.1f;
+			// On enregistre un pourcentage négatif
+			// On s'attend à recevoir une exception
+			r.salaireMensuel(0, POURCENTAGE);
+			// Si on arrive ici, le test doit échouer.Il s'agit d'une erreur.
+                        // On force l'échec du test
+			fail("Un pourcentage négatif doit générer une exception"); 			
+		} catch (IllegalArgumentException e) {
+			//Si on arrive ici cela correspond au comportement attendu : c'est normal.
+		}
+	}
+        
+        
+        @Test
+        public void register()
+        {
+            float CA = 50000f;
+            //On enregistre un CA pour le mois zero
+            r.registerCA(0, CA);
+            assertEquals(CA, r.getCAMensuel()[0], "Le CA enregistré n'est pas le bon");
+            
+        }
+        
+        @Test
+        public void registerMonth()
+        {
+		try {
+			// On enregistre le mois -1
+			// On s'attend à recevoir une exception
+			r.registerCA(-1, 5000f);
+			// Si on arrive ici, c'est une erreur, le test doit échouer
+			fail("Un mois négatif doit lever une exception"); // Forcer l'échec du test			
+		} 
+                catch (IllegalArgumentException e) 
+                {
+			// Si on arrive ici, c'est normal, c'est le comportement attendu
+		}
+        }
+        
+        
 	@Test
 	public void testCANegatifImpossible() {
 		
 		try {
 			// On enregistre un CA négatif, que doit-il se passer ?
 			// On s'attend à recevoir une exception
-			r.enregistrerCA(0, -10000f);
+			r.registerCA(0, -10000f);
 			// Si on arrive ici, c'est une erreur, le test doit échouer
 			fail("Un CA négatif doit générer une exception"); // Forcer l'échec du test			
-		} catch (IllegalArgumentException e) {
+		} 
+                catch (IllegalArgumentException e) 
+                {
 			// Si on arrive ici, c'est normal, c'est le comportement attendu
 		}
 
 	}
 	
-	
+	@Test
+	public void testCAMensuel() 
+        {
+		try {
+                        float[] CA13 = new float[13];
+			// On enregistre treize mois de CA
+			// On s'attend à recevoir une exception
+			r.setCAMensuel(CA13);
+			// Si on arrive ici, c'est une erreur, le test doit échouer
+                        // On force l'échec du test
+			fail("On ne peut pas enregistrer plus de 12 valeurs dans CAMensuel"); 		
+		} catch (IllegalArgumentException e)
+                {
+			// Si on arrive ici, c'est normal, c'est le comportement attendu
+		}
+
+	}
+        
+        @Test
+        public void testgetCAMensuel() 
+        {
+                float[] CA2 = {5000f,6000f};
+	        r.setCAMensuel(CA2);
+                assertEquals(CA2, r.getCAMensuel(), "Le CA retourné n'est pas le bon.");
+	}
+       
+       
+        //Test ToString
+         @Test
+	public void testToStringL() {
+            assertEquals("Representant{numero=36, nom=Bastide, prenom=Rémi}", r.toString(), "Le toString n'affiche pas le bon message");
+	}
 }
